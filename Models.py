6870e2958b5c3e5c60ad54c8905b1a6294ee970a
henry-dev-atkins <henryat44@gmail.com>
2023-01-31 22:48:02 +0000

    Added methods and tidied repo in prep for progress report.

---
+++ Models.py
@@ -0,0 +1,95 @@
+from DataGenerate import *
+
+word_pairs = GetWordPairs()
+word_list = GetWords()
+
+class W2VModel():
+	def __init__(self, vector_dims, dir='C:\\Users\\henry\\Desktop\\MastersProject\\QNLP_MasterWork\\Word2VecModels\\brown_model_', printing=False):
+		self.vector_dims = vector_dims
+		if not os.path.exists(dir+str(vector_dims)):
+			self.model = gensim.models.Word2Vec(brown.sents(), min_count=1,vector_size=vector_dims, window=5, workers=4)
+			self.model.save(dir+str(vector_dims))
+			if printing:print("Made Word2Vec Model of ", str(vector_dims), " dimensions")
+		else:
+			self.model = Word2Vec.load(dir+str(vector_dims))
+			if printing:print("Load Word2Vec Model of ", str(vector_dims), " dimensions")
+		
+	def getvector(self, words):
+		return self.model.wv[words]
+
+class BertEncodingWords():
+	#https://colab.research.google.com/drive/1yFphU6PW9Uo6lmDly_ud9a6c4RCYlwdX#scrollTo=Zn0n2S-FWZih
+	
+	def __init__(self):
+		# Load pre-trained model tokenizer (vocabulary)
+		self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+		self.model = BertModel.from_pretrained('bert-base-uncased',output_hidden_states = True).eval()# output_hidden_states is whether the model returns all hidden-states. # Put the model in "evaluation" mode, meaning feed-forward operation. 
+		pass
+	
+	def getWordEmbedding(self, word):
+		marked_text = "[CLS] " + word + " [SEP]"
+		tokenized_text = self.tokenizer.tokenize(marked_text)	# Tokenize our sentence with the BERT tokenizer.
+		return(tokenized_text)	# Print out the tokens.
+	
+	def preprocessSentence(self, word):
+		# Add the special tokens.
+		tokenized_text = self.getWordEmbedding(word)
+		# Map the token strings to their vocabulary indeces.
+		indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+
+		# Mark each of the 22 tokens as belonging to sentence "1".
+		segments_ids = [1] * len(tokenized_text)
+
+		# Convert inputs to PyTorch tensors
+		tokens_tensor = torch.tensor([indexed_tokens])
+		segments_tensors = torch.tensor([segments_ids])
+		return tokens_tensor, segments_tensors
+	
+	def hiddenLayersBERT(self, word):
+		hidden_states = [] 
+		with torch.no_grad():
+			outputs = self.model(word[0])
+			# https://huggingface.co/transformers/model_doc/bert.html#bertmodel
+			hidden_states.append(outputs[2])
+		return hidden_states
+
+	def GetEmbeddingsForWord(self, hidden):
+		# Stores the token vectors, with shape [22 x 768]
+		token_vecs_sum = []
+
+		# `token_embeddings` is a [22 x 12 x 768] tensor.
+
+		# For each token in the sentence...
+		for token in hidden:
+			# `token` is a [12 x 768] tensor
+			# Sum the vectors from the last four layers.
+			sum_vec = torch.sum(token[0][0], dim=0)
+			
+			# Use `sum_vec` to represent `token`.
+			token_vecs_sum.append(sum_vec)
+		token_embedding_list = token_vecs_sum
+		return token_embedding_list
+	
+	def GetTokenVecSum(self, hidden_state):
+		# Stores the token vectors, with shape [22 x 768]
+		token_vecs_cum_sum = []
+
+		# `token_embeddings` is a [22 x 12 x 768] tensor.
+
+		# For each token in the sentence...
+		for token in hidden_state:
+
+			# `token` is a [12 x 768] tensor
+			# Sum the vectors from the last four layers.
+			sum_vec = torch.sum(token[0][0], dim=0)
+			
+			# Use `sum_vec` to represent `token`.
+			token_vecs_cum_sum.append(sum_vec)
+		token_vecs_sum = token_vecs_cum_sum
+		return token_vecs_sum
+
+	def getWordVector(self, word):
+		word_to_evaluate = self.preprocessSentence(word)
+		hidden = self.hiddenLayersBERT(word_to_evaluate)
+		embeddings = self.GetEmbeddingsForWord(hidden)
+		return np.array(embeddings[0])
